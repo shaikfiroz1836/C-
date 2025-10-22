11ecefc1c38bbf73982523f8ea19b42bec04b983
shaikfiroz1836 <shaikfiroz1836@gmail.com>
2025-10-22 11:47:35 +0530

    First commit

---
+++ Demo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Demo
+{
+    class Program
+    {
+        int i;
+        string s;
+        bool b;
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            Console.WriteLine("value of i: " + p.i);
+            Console.WriteLine("value of s: " + p.s);
+            Console.WriteLine("value of b: " + p.b);
+            Console.WriteLine();
+        }
+    }
+}
