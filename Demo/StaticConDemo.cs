475a2584aabd926502c2bc09495580961a0624fe
shaikfiroz1836 <shaikfiroz1836@gmail.com>
2025-10-23 13:14:46 +0530

    tst

---
+++ Demo/StaticConDemo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    internal class StaticConDemo
+    {
+        static StaticConDemo()
+        {
+            Console.WriteLine("Static Constructor Called");
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Main Method Called");
+        }
+    }
+}
