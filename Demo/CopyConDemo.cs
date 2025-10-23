b3a8afad5188dd72c14d50e9e60b73dbea518e0f
shaikfiroz1836 <shaikfiroz1836@gmail.com>
2025-10-23 12:26:59 +0530

    git push

---
+++ Demo/CopyConDemo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    internal class CopyConDemo
+    {
+        int x;
+        public CopyConDemo(int i)
+        {
+            x = i;
+        }
+        public void Display()
+        {            
+            Console.WriteLine("The value of x is : " + x);
+        }
+        static void Main(string[] args)
+        {
+            CopyConDemo a = new CopyConDemo(10);
+            a.Display();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Demo/ParameterConDemo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    internal class ParameterConDemo
+    {
+        int x;
+        public ParameterConDemo(int a)
+        {
+            x = a;
+            Console.WriteLine("Parameterized Constructor: " + a);
+        }
+        public void Display()
+        {
+            Console.WriteLine("The value of x is : " + x);
+        }
+        static void Main() 
+        {
+            ParameterConDemo p = new ParameterConDemo(100);
+            ParameterConDemo p1 = new ParameterConDemo(1);
+            ParameterConDemo p2 = new ParameterConDemo(1200);
+            p.Display();
+            p1.Display();
+            p2.Display();
+
+        }
+    }
+}
