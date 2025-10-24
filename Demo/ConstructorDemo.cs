63f92e34bcf0b646002647ce1d77ffb65956257c
shaikfiroz1836 <shaikfiroz1836@gmail.com>
2025-10-24 12:53:24 +0530

    Test

---
+++ Demo/ConstructorDemo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    internal class ConstructorDemo
+    {
+        static int y;
+        int x;
+        static ConstructorDemo()
+        {
+            Console.WriteLine("Static Constructor is Called");
+        }
+        public ConstructorDemo(int x)
+        {
+            this.x = x;
+            Console.WriteLine("Non-Static Constructor is Called");
+        }
+        static void Main()
+        {
+            
+            Console.WriteLine("Main Method is called");
+            ConstructorDemo a1 = new ConstructorDemo(1);
+            ConstructorDemo a2 = new ConstructorDemo(2);
+            ConstructorDemo a3 = new ConstructorDemo(3);
+
+            Console.WriteLine(y);
+            Console.WriteLine(a1.x + " "+a2.x+" "+a3.x);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace Test
+{
+    class Program
+    {
+        int x = 100; //x is Variable of a class
+        static void Main(string[] args)
+        {
+            Program p = new Program();  //p is Instance of a class
+            Program p1 = p;    //p1 is Reference of a class        
+            Console.WriteLine(p.x);
+            Console.WriteLine(p1.x);
+            Console.WriteLine(p.x +" "+ p1.x);
+        }
+    }
+}
