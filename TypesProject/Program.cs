9ccfb622242685207afeba1d7ab633ad9868ba26
shaikfiroz1836 <shaikfiroz1836@gmail.com>
2025-10-27 12:18:29 +0530

    Diff Variables of a class

---
+++ TypesProject/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace TypesProject
+{
+    class Program
+    {
+        int i;
+        static int j = 200;
+        const float pi = 3.14f;
+        readonly bool flag;
+        public Program(int i,bool flag)
+        {
+            this.i = i;
+            this.flag = flag;
+        }
+        static void Main()
+        {
+            Program p1 = new Program(50,true);
+            Program p2 = new Program(150,false);
+            Console.WriteLine(p1.i +" "+p2.i);
+            Console.WriteLine(j);
+            Console.WriteLine(pi);
+            Console.WriteLine(p1.flag + " " + p2.flag);
+            p1.i = 50000;
+            Console.WriteLine(p1.i+" "+p2.i);
+        }
+    }
+}
