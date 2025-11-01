0cb55537ac675626f62487d7a5118da6102a7930
shaikfiroz1836 <shaikfiroz1836@gmail.com>
2025-11-01 12:10:22 +0530

    Inheritance

---
+++ Inheritance/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Inheritance
+{
+    class Class1
+    {
+        public void Test1()
+        {
+            Console.WriteLine("Test 1 method");
+        }
+        public void Test2()
+        {
+            Console.WriteLine("Test 2 method");
+        }
+        static void Main()
+        {
+
+        }
+    }
+}
---
+++ Inheritance/Class2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    internal class Class2 : Class1
+    {
+        static void Main()
+        {
+            Class2 c = new Class2();
+            c.Test1();
+            c.Test2();
+        }
+    }
+}
