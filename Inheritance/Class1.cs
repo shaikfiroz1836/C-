77c4de47c026a138c26d5293bb591fc78eb72ff8
shaikfiroz1836 <shaikfiroz1836@gmail.com>
2025-11-02 12:00:48 +0530

    Inheritance

--- Inheritance/Class1.cs
+++ Inheritance/Class1.cs
@@ -4,9 +4,9 @@ namespace Inheritance
 {
     public class Class1
     {
-        public Class1()
+        public Class1(int i)
         {
-            Console.WriteLine("Class1 Constructor is called");
+            Console.WriteLine("Class1 Constructor is called: "+ i);
         }
         public void Test1()
         {
--- Inheritance/Class2.cs
+++ Inheritance/Class2.cs
@@ -8,9 +8,9 @@ namespace Inheritance
 {
     internal class Class2 : Class1
     {
-        Class2()
+        public Class2(int a) : base(a)
         {
-            Console.WriteLine("Class2 Constructor is called");
+            Console.WriteLine("Class2 Constructor is called: "+a);
         }
         public void Test3()
         {
@@ -18,10 +18,17 @@ namespace Inheritance
         }
         static void Main()
         {
-            Class2 c = new Class2();
-            c.Test1();
-            c.Test2();
-            c.Test3();
+            //Object obj = new Object();
+            //Console.WriteLine(obj.GetType);
+            //Class2 c = new Class2();
+            //c.Test1();
+            //c.Test2();
+            //c.Test3();
+            //Class1 c1 = new Class1();
+            //Console.WriteLine(c1.GetType());
+            Class2 c2 = new Class2(50);
+            //Console.WriteLine(c2.GetType());
+
         }
     }
 }
