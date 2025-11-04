b65d202c5f6f7d6d3f316b7157f44973922c80a6
shaikfiroz1836 <shaikfiroz1836@gmail.com>
2025-11-04 13:02:42 +0530

    Method Overriding

--- MethodOverriding/LoadParent2.cs
+++ MethodOverriding/LoadParent2.cs
@@ -2,26 +2,30 @@
 
 namespace MethodOverriding
 {
-    public class LoadParent
+    public class LoadParent2
     {
         public void Show()
         {
             Console.WriteLine("Parent Show method is called.");
         }
-        public void Test()
+        public virtual void Test()
         {
             Console.WriteLine("Parent Test method is called.");
         }
     }
-    public class LoadChild : LoadParent
+    public class LoadChild2 : LoadParent2
     {
         public void Show(int i)
         {
             Console.WriteLine("Child Show method is called.");
         }
+        public override void Test()
+        {
+            Console.WriteLine("Child Test method is called.");
+        }
         static void Main()
         {
-            LoadChild c = new LoadChild();
+            LoadChild2 c = new LoadChild2();
             c.Show();
             c.Show(10);
             c.Test();
