2817c0ac1e1551db712d07996a1674d9d3c31a3f
shaikfiroz1836 <shaikfiroz1836@gmail.com>
2025-11-04 11:42:15 +0530

    Method Overriding

---
+++ MethodOverriding/LoadParent.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MethodOverriding
+{
+    public class LoadParent
+    {
+        public void Show()
+        {
+            Console.WriteLine("Parent Show method is called.");
+        }
+        public void Test()
+        {
+            Console.WriteLine("Parent Test method is called.");
+        }
+    }
+    public class LoadChild : LoadParent
+    {
+        static void Main()
+        {
+            LoadChild c = new LoadChild();
+            c.Show();
+            c.Test();
+        }
+    }
+}
