83f708a32a0a21e00f56b622a8a257604d27120a
shaikfiroz1836 <shaikfiroz1836@gmail.com>
2025-11-05 12:27:50 +0530

    Method Hiding

---
+++ MethodHiding/ParentClass.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace MethodHiding
+{
+    class ParentClass
+    {
+        public virtual void Test1()
+        {
+            Console.WriteLine("ParentClass Test1");
+        }
+        public void Test2()
+        {
+            Console.WriteLine("ParentClass Test2");
+        }
+    }
+    class ChildClass : ParentClass 
+    {
+        public override void Test1()
+        {
+            Console.WriteLine("ChildClass Test1");
+        }
+        public new void Test2()
+        {
+            Console.WriteLine("ChildClass Test2");
+        }
+        static void Main()
+        {
+            ChildClass c = new ChildClass();
+            c.Test1();
+            c.Test2();
+        }
+    }
+}
