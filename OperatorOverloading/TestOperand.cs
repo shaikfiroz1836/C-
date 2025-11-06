d2170aaddfa20032f91f61c2e93011e4edd35379
shaikfiroz1836 <shaikfiroz1836@gmail.com>
2025-11-06 12:44:54 +0530

    Operator Overloading

---
+++ OperatorOverloading/TestOperand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace OpetaorOverloading
+{
+    class TestOperand { 
+        
+
+        static void Main()
+        {
+            string s1 = "Hello";
+            string s2 = "World";
+            Console.WriteLine(s1 + s2);
+        }
+    }
+}
